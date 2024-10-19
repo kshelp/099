bf65a7ea119b7eccff336eb01410da3c5618c1e9
Max Lee <kshelp@naver.com>
2024-10-19 20:05:46 +0000

    Update floatingPopulationList.js

--- src/components/Floating_population/floatingPopulationList.js
+++ src/components/Floating_population/floatingPopulationList.js
@@ -17,8 +17,7 @@ class floatingPopulationList extends Component {
 
     callFloatPopulListApi = async () => {
         try {
-            let apiUrl = process.env.REACT_APP_API_URL;
-            // apiUrl = "http://192.168.1.12:8080";
+            const apiUrl = process.env.REACT_APP_API_URL;
             alert("apiUrl="+apiUrl);
             const response = await axios.get(`${apiUrl}/api/dept`);
             // 성공적으로 데이터를 가져왔을 때
